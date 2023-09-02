dad80df5ce2ff842f5c2773645ecae0ff99e3348
Shinsei <murakami.shinsei.zn@gmail.com>
2023-09-02 09:56:22 +0900

    非同期-indexedDB

---
+++ async-db/src/App.vue
@@ -0,0 +1,39 @@
+
+
+<template>
+  <header>
+    <h1>Piniaサンプル</h1>
+  </header>
+  <main>
+    <RouterView/>
+  </main>
+</template>
+
+<style>
+  #app {
+    display: block;
+  }
+  main {
+    border: 1px solid blue;
+    padding: 10px;
+  }
+  #breadcrumb ul li {
+    display: inline;
+    list-style-type: none;
+  }
+  #breadcrumb { 
+    margin-left: 0px;
+  }
+  #breadcrumb ul {
+    padding-left: 0px;
+  }
+  #breadcrumb ul .current {
+    color: red;
+  }
+  #breadcrumb ul li::before {
+    content: " > ";
+  }
+  #breadcrumb ul li:first-child::before {
+    content: none;
+  }
+</style>
